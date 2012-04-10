5cdb07b4d45a89fc03a214610120de130cb530a0
Keiichiro Ui <keiichiro.ui@gmail.com>
2012-04-11 02:22:45 +0900

    initialize for writing input event

--- lib/revdev/input_event.rb
+++ lib/revdev/input_event.rb
@@ -1,7 +1,46 @@
 # -*- coding:utf-8; mode:ruby; -*-
 
 module Revdev
+
+  # wraper of "struct input_event" on "input.h"
   class InputEvent
+
+    # arg: String(byte string), Hash, Time
+    # type, code, value: Integer (ignored values if arg is String or Hash)
+    def initialize arg, type = nil, code = nil, value = nil
+      if arg.instance_of? String
+        raw_initialize arg
+      elsif arg.instance_of? Hash
+        @time = arg['time'] || arg[:time] || Time.now
+        @type = arg['type'] || arg[:type]
+        @code = arg['code'] || arg[:code]
+        @value = arg['value'] || arg[:value]
+      else
+        @time = arg || Time.now
+        @type = type
+        @code = code
+        @value = value
+      end
+    end
+
+    # human readable @type of input event
+    def hr_type
+      REVERSE_MAPS[:EV][@type]
+    end
+
+    # human readable @code of input event
+    def hr_code
+      ht = hr_type
+      return nil if ht.nil?
+      map = REVERSE_MAPS[ht]
+      map && map[@code]
+    end
+
+    # human readable @code of input event
+    # not implement
+    def hr_value
+      nil
+    end
 
     PREFIX_CONVERTER = {
       'BTN'	=> 'KEY',
@@ -29,20 +68,5 @@ module Revdev
       REVERSE_MAPS[ev][const_get const] = const.to_sym
     end
 
-    # human readable @type of input event
-    def hr_type
-      REVERSE_MAPS[:EV][@type]
-    end
-
-    # human readable @code of input event
-    def hr_code
-      ht = hr_type
-      return nil if ht.nil?
-      map = REVERSE_MAPS[ht]
-      map && map[@code]
-    end
-
-    # human readable @code of input event
-    # def hr_value; end
   end
 end
