b8ab7c3e37f7e1d7ca3ae4b643cbee8334fce61c
Keiichiro Ui <keiichiro.ui@gmail.com>
2013-04-17 19:46:56 +0900

    add a macro to specific the ruby minor version

--- ext/revdev/extconf.rb
+++ ext/revdev/extconf.rb
@@ -1,3 +1,12 @@
 require "mkmf"
 
+case RUBY_VERSION
+when '1.8' .. '1.9'
+  $defs << '-DRUBY_1_8'
+when '1.9' .. '2.0'
+  $defs << '-DRUBY_1_9'
+else
+  abort "Not supported ruby-version: #{RUBY_VERSION}"
+end
+
 create_makefile "revdev/revdev"
