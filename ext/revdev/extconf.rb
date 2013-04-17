bc3a5a31b7e44f5faebb8e3bc3cd022d85e05c69
Keiichiro Ui <keiichiro.ui@gmail.com>
2013-04-17 22:45:02 +0900

    add micro definition to support both ruby 1.8 and 1.9

--- ext/revdev/extconf.rb
+++ ext/revdev/extconf.rb
@@ -1,12 +1,18 @@
 require "mkmf"
 
-case RUBY_VERSION
-when '1.8' .. '1.9'
-  $defs << '-DRUBY_1_8'
-when '1.9' .. '2.0'
-  $defs << '-DRUBY_1_9'
-else
+# version => micro
+map = {
+  '1.8.' => 'RUBY_1_8',
+  '1.9.' => 'RUBY_1_9',
+}
+version, micro = map.find do |v, m|
+  RUBY_VERSION.start_with? v
+end
+
+if micro.nil?
   abort "Not supported ruby-version: #{RUBY_VERSION}"
+else
+  $defs << "-D#{micro}"
 end
 
 create_makefile "revdev/revdev"
